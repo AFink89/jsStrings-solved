88b24ed344350132dcfc5edb28f9f7a3ca6ea3f2
AMaupin <alexandrafink8989@gmail.com>
2025-07-26 12:33:01 -0700

    add boilerplate and script link in index.html, add var items, create string to make sentence, console.log

---
+++ index.js
@@ -0,0 +1,15 @@
+var firstName = 'Alex';
+var lastName = 'Maupin';
+var birthDay = 29;
+var birthMonth = 'December';
+var birthYear = 1989;
+var age = 35;
+var isOrganDonor = false;
+var streetAddressNumber = 4411;
+var streetAddressName = ' Newton Street';
+var city = 'Denver';
+var state = 'CO';
+
+var introduceSelf = 'Hello, my name is ' + firstName + ' ' + lastName + '. ' + 'My birthday is ' + birthMonth + ' ' + birthDay + ', ' + birthYear + '. ' + 'My childhood home is ' + streetAddressNumber + streetAddressName + ' in ' + city + ', ' + state;
+
+console.log(introduceSelf);
